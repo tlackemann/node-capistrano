f620bd327f3fd672c01f1bc8afec57b28b87d9c8
Thomas Lackemann <tommylackemann@gmail.com>
2014-10-14 07:19:29 -0400

    added capistrano

--- server.js
+++ server.js
@@ -3,6 +3,6 @@
   http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
       res.end('Hello World\n');
-      }).listen(1337, '127.0.0.1');
-      console.log('Server running at http://127.0.0.1:1337/');
+      }).listen(process.env.PORT || 1337);
+      console.log('Server running on port ' + (process.env.PORT || 1337));
 })();
